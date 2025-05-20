90dc714d1bbf8872631bcf0e585cf1160bda0849
themelnykanton <themelnykanton@gmail.com>
2025-05-20 20:05:08 +0200

    added-scroll-to-sections

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -3,6 +3,27 @@ document.addEventListener("DOMContentLoaded", function () {
   const burgerButton = document.querySelector(".header-burger-button");
   const closeButton = document.querySelector(".close-navbar-wrapper");
   const navbarOverlay = document.querySelector(".navbar-overlay");
+  const navLinks = document.querySelectorAll('.navbar-list-link');
+  const body = document.body;
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      if (targetSection) {
+        targetSection.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+
+      navbarOverlay.classList.remove('is-visible');
+      body.classList.remove('body-no-scroll');
+    });
+  });
 
   burgerButton.addEventListener("click", () => {
     navbarOverlay.classList.add("is-visible");
@@ -11,6 +32,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
   closeButton.addEventListener("click", () => {
     navbarOverlay.classList.remove("is-visible");
+    document.body.classList.remove('body-no-scroll');
+  });
+
+  navbarSections.addEventListener("click", () => {
+    navbarOverlay.classList.add("is-hidden")
     document.body.classList.remove('body-no-scroll');
   });
 
